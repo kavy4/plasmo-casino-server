c68ed2d756bcb826dce18bed0ccd60633b60b2b7
kavy4 <tyty020408@gmail.com>
2023-01-15 18:45:13 +0300

    commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const { response } = require('express')
+const app = express()
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server worked: http://localhost:${PORT}`))
+
+app.use(cors())
+app.use(express.json())
+
+const userList = []
+
+app.get('/api/get-user', (request, response) => {
+    response.json(JSON.stringify(userList))
+})
+
+app.post('/api/register', (request, response) => {
+    userList.push(request.body)
+    console.log(userList)
+})
+
+app.get('/api/clear-user', (request, response) => {
+    userList.length = 0
+    response.json('UserArray clear')
+    console.log(userList)
+})
